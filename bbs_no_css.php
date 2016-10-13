b603e7992ee0bc8c43977206a9eb2468314d2b0d
DESKTOP-810S26R\jdaif <dai.kanto20@gmail.com>
2016-10-13 10:44:42 +0800

    降順でのコメントの表示

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -22,10 +22,45 @@ $stmt = $dbh->prepare($sql);
 $stmt->execute();
 
 }
+
+//sql文の作成
+//$sql ='SELECT*FROM`posts`';
+$sql ="SELECT `id`, `nickname`, `comment`, `created` FROM `posts` ORDER BY`created` DESC";
+
+
+//secect文実行
+
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+
+//変数にDBから取得したデータを格納→フェッチ　連想配列に変更するため　また複雑な処理を行うため
+
+
+
+//格納する変数の初期化
+$posts=array();
+
+//繰り返し文でのデータ取得
+while (1) {
+	$rec=$stmt->fetch(PDO::FETCH_ASSOC);
+	if($rec==false){
+		break;
+	}
+//取得したデータを配列に格納しておく
+	$posts[]=$rec;
+
+}
+
+
+
 // ３．データベースを切断する
 $dbh = null;
 
 ?>
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -41,6 +76,30 @@ $dbh = null;
       <p><button type="submit" >つぶやく</button></p>
     </form>
     <!-- ここにニックネーム、つぶやいた内容、日付を表示する -->
+    <!--<ul>
+    	<li><?php //echo $posts[0] ['nickname'];?> 2016/10/13</li>
+    	<li>nick name comment 2016/10/12</li>
+    	<li>nick name comment 2016/10/11</li>
+    </ul>>
+    -->
 
+    <ul>
+    <?php
+    	foreach ($posts as $post_each) {
+    		echo '<li>';
+    		echo $post_each['nickname'].' ';
+    		echo $post_each['comment'].' ';
+
+    		//日付型に変換
+    		$created= strtotime($post_each['created']);
+
+    		//書式を変換
+    		$created= date('Y/m/d', $created);
+
+    		echo $created;
+    		echo '</li>';
+    	}
+    ?>
+    </ul>
 </body>
 </html>
